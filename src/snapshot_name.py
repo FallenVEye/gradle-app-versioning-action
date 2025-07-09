3b1070facd4d13b1db33682b6d8f7b320a6705a0
Vlad Glazunov <vlad.glazunov.02@gmail.com>
2025-07-09 22:17:54 +0300

    add option for unique snapshot

--- src/snapshot_name.py
+++ src/snapshot_name.py
@@ -1,9 +1,16 @@
 
+import os
 from git import Repo
 
 def getSnapshotName():
-    repo_name = Repo().active_branch.name
+    is_unique = os.getenv("INPUT_UNIQUE_SNAPSHOT")
+
+    repo = Repo()
+    repo_name = repo.active_branch.name
     if not repo_name.startswith("feature/"):
         return ""
     else:
-        return repo_name.removeprefix("feature/").replace("/", "-").replace("_", "-")
+        snapshot_name = repo_name.removeprefix("feature/").replace("/", "-").replace("_", "-")
+        if is_unique:
+            snapshot_name += str(repo.head.commit)
+        return snapshot_name
