5323786b63b43c8566b15f0353244c4ba13ad60e
Vlad Glazunov <vlad.glazunov.02@gmail.com>
2025-07-09 22:39:54 +0300

    warn if GITHUB_OUTPUT is not set

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 from configobj import ConfigObj
 from pystreamapi import Stream
 from snapshot_name import getSnapshotName
+import warnings
 
 version_type = os.getenv("INPUT_VERSION_TYPE").casefold() 
 is_snapshot = os.getenv("INPUT_SNAPSHOT").casefold() == "true"
@@ -33,5 +34,8 @@ config.write()
 save_to_gh_output = os.getenv("INPUT_SAVE_VERSION_TO_GH_OUTPUT").casefold() == "true"
 
 if (save_to_gh_output):
-    file = open(os.getenv("GITHUB_OUTPUT"))
+    path = os.getenv("GITHUB_OUTPUT")
+    if path is None:
+        warnings.warn("$GITHUB_OUTPUT is null")
+    file = open(path)
     file.write("version=" + newVersion + "\n")
